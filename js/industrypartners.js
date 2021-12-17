22d6961077bc2e9251bbb272df29af9c136c7223
Sahil Ghauri <sghauri@albany.edu>
2021-12-17 11:27:18 -0500

    changed instances of wiser to wecliemprep-dev

--- js/industrypartners.js
+++ js/industrypartners.js
@@ -1,4 +1,4 @@
-let requestURL = "https://sdat-dev.github.io/resources/wiser/data/industrypartners.json";
+let requestURL = "https://sdat-dev.github.io/resources/wecliemprep-dev/data/industrypartners.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
