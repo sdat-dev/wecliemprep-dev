840787b3d3f18f86b6a2d39c1db191e48637d638
TheSahil <sghauri@albany.edu>
2021-12-17 16:15:53 -0500

    auto commit due to data change

--- js/academicfounders.js
+++ js/academicfounders.js
@@ -1,4 +1,4 @@
-let requestURL = "data/academicfounders.json";
+let requestURL = "https://sdat-dev.github.io/resources/wecliemprep-dev/data/academicfounders.json";
 let request = new XMLHttpRequest();
 //getting content Element to append grants information
 let maincontentContainer = document.getElementsByClassName('main-content')[0];
